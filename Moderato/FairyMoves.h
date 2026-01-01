2516a9b1a80562d9605c89577af219c2cbda9b6b
Ivan <155380451+denkovskion@users.noreply.github.com>
2026-01-01 02:08:22 +0100

    Increment year.

--- Moderato/FairyMoves.h
+++ Moderato/FairyMoves.h
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2025 Ivan Denkovski
+ * Copyright (c) 2025-2026 Ivan Denkovski
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
